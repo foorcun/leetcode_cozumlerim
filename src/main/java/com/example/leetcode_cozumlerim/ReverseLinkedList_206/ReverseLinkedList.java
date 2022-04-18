99a8d2d0d7e71ea3917de0aa418b922dbe33e742
foorcun <msnifoorcun@hotmail.com>
2022-04-18 21:57:26 +0300

    Commentler ve linkler eklendi

--- src/main/java/com/example/leetcode_cozumlerim/ReverseLinkedList_206/ReverseLinkedList.java
+++ src/main/java/com/example/leetcode_cozumlerim/ReverseLinkedList_206/ReverseLinkedList.java
@@ -10,24 +10,27 @@ package com.example.leetcode_cozumlerim.ReverseLinkedList_206;
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// link: https://www.youtube.com/watch?v=G0_I-ZF0S38 // neetcode
+// link: https://www.youtube.com/watch?v=jY-EUKXYT20&t=257s // guzel gorsel anlatim
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null){ // Bitirme sarti
+        if(head == null){ // bu kod "bos" linked list pass edilirse diye bir check.
+            // yani pass edilen LinkedList en az bir uyeye(head) sahip olmalidir sarti.
             return head;
         }
 
         ListNode prev = null; // left pointer
         ListNode curr = head; // right pointer
 
-        while(curr != null){
+        while(curr != null){ //bu sart, curr pointer son elemanida gecerse null olur ve LinkedList biter.;
             //burda Swap olayi yapiliyor
-            ListNode temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
+            ListNode temp = curr.next; // bu suanki next Nodu elde tutuyor
+            curr.next = prev; // bu curr daki Nodu umuzun ok yonunu ters ceviriyor
+            prev = curr; // prev bir adim ilerler
+            curr = temp; // curr bir adim ilerler
 
         }
-        return prev;
+        return prev; // curr == null => artik prev eski son Node, yeni Head demektir.
 
     }
 }
