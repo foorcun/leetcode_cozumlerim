99d57d38957ef561578281396f3ca6c7cbf50204
foorcun <msnifoorcun@hotmail.com>
2022-04-21 16:08:58 +0300

    FindtheDuplicateNumber287 solution eklendi

---
+++ src/main/java/com/example/leetcode_cozumlerim/FindtheDuplicateNumber287/Solution.java
@@ -0,0 +1,29 @@
+package com.example.leetcode_cozumlerim.FindtheDuplicateNumber287;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+
+        if(nums.length==0){
+            return 0;
+        }
+
+        int slow =nums[0]; // pointer ilk elementte basliyor
+        int fast = nums[nums[0]]; // pointer ikinci elementte basliyor
+
+        while(slow != fast){ // finding cycle exist
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        // finding kesisen nokta
+        // slow reset
+        slow =0;
+        while (true){
+            slow = nums[slow];
+            fast = nums[fast]; // fast pointer going to be slow now
+
+            if(slow == fast) return slow;
+
+        }
+    }
+}
